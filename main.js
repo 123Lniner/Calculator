7507ad18e55a8e8ca7ab8b3ab297f2551654a3e2
123Lniner <164357272+123Lniner@users.noreply.github.com>
2024-03-26 09:20:01 -0400

    Update index.html, style.css, and main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Calculator code here!
+// Calculator script here! You may copy this and edit the copy at your will.
+
+const display = document.getElementById("display");
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value = "";
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
