6ebcce1a2165318f5ccb71926c19e4341c4f5647
123Lniner <164357272+123Lniner@users.noreply.github.com>
2024-03-26 19:21:09 -0400

    Rework index.html, style.css, and main.js

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-// Calculator script here! You may copy this and edit the copy at your will.
+// Console Logger
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
 
-const display = document.getElementById("display");
-function appendToDisplay(input){
-    display.value += input;
-}
-function clearDisplay(){
-    display.value = "";
-}
-function calculate(){
-    try{
-        display.value = eval(display.value);
+// These "specialChars" are the operator keys.
+const specialChars = ["%", "/", "*", "-", "+", "="];
+
+// This constant defines a function based on which button is clicked
+const calculate = (btnValue) => {
+    if(btnValue === "=" && btnValue !== ""){
+        // If 'output' has '%', replace '%' with '/100' before calculating.
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC"){
+        output = "";
+    } else if (btnValue === "DEL"){
+        // If 'DEL' button is clicked, the calculator removes the last character from 'output'.
+        output = output.toString().slice(0, -1);
+    } else {
+        //If 'output' is empty and button pressed is part of 'specialChars' then calculator won't record character
+        if (output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
     }
-    catch(error){
-        display.value = "Error";
-    }
-}
+    display.value = output;
+};
+
+// Event listener for buttons calls calculate() on click + dataset values as argument.
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => console.log(e.target.dataset.value));
+});
